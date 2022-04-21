d34ba30fb0e7c27cde83324b24545c1c9f2c5187
@Marufjonov <krasafchik1717@mail.ru>
2022-04-21 10:41:11 +0500

    Initial commit

---
+++ app/src/main/java/com/marufjonov/toastylib/MainActivity.java
@@ -0,0 +1,16 @@
+package com.marufjonov.toastylib;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toaster.simpleToast(this, "asdfdsf");
+    }
+}
